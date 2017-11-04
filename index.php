9c0ec871beea8d9bad96ac8339f7d5bcf2ee4747
jmoostland <jeanineoostland@gmail.com>
2017-11-04 14:46:26 +0100

    eerste commit

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
         <title>Zeeslag</title>
 
         <script>
+            var boten=["1","2","3","4","5","6","7","8","9","10"];
             function schieten(mis) {
-                document.getElementById('nummer' + mis).value = "water";
-
+                document.getElementById('nummer' + mis).value=[boten[x]];
             }
             function schieten1(mis) {
                 document.getElementById('nr1' + mis).value = "water";
@@ -18,23 +18,28 @@
         </script>
 
         <style>
+            
+            .button{
+                /*                display: box;*/
+                color: whitesmoke;
+                width: 50px;
+                height: 50px;
+                cursor: crosshair;
+                background-image:url(water-banner.jpg);
+            }
             .button:hover{
                 box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
                 background-image:url(bom.jpg);
                 width: 50px;
                 height: 50px;
             }
-
-            .button {
-/*                display: box;*/
+            .button:active{
+                background-image:url(bom.jpg);
                 width: 50px;
                 height: 50px;
-                cursor: crosshair;
-                background-image:url(water-banner.jpg);
             }
-            
-            .button:after{
-                background-color: aqua;
+            .button:visited{
+                background-image:url(water-banner.jpg);
                 width: 50px;
                 height: 50px;
             }
@@ -46,17 +51,16 @@
 
 
         <?php
-        $plaatsen=array("boot","boot","boot","water","water","water","water","water","water","water");
         for ($x = 0; $x < 10; $x++) {
-            echo "<input type=button onclick=schieten(" .$x. ") class=button value='.' id=nummer" . $x . ">";
+            echo "<input type=button onclick=schieten(" . $x . ") class=button value='.'id=nummer" . $x . ">";
         }
         echo "<br>";
         for ($x = 0; $x < 10; $x++) {
-            echo "<input type=button onclick=schieten1(" . $x . ") class=button value=plaatje id=nr1" . $x . ">";
+            echo "<input type=button onclick=schieten1(" . $x . ") class=button value='.'id=nr1" . $x . ">";
         }
         echo "<br>";
         for ($x = 0; $x < 10; $x++) {
-            echo "<input type=button onclick=schieten2(" . $x . ") class=button value=plaatje id=nr2" . $x . ">";
+            echo "<input type=button onclick=schieten2(" . $x . ") class=button value='.'id=nr2" . $x . ">";
         }
         echo "<br>";
         ?>
